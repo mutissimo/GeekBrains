e4a666096528a3485c33a62138f568746b44e716
mutissimo <mutissimo@gmail.com>
2021-03-19 12:42:44 +0300

    lesson-3

---
+++ 1. C_sharp_Base/Lesson-3/Task-3-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] adressbook = new string[,] { { "Иванов", "Петров", "Сидоров", "Путин", "Медведев" }, { "123", "456", "789", "147", "258" } };
+            for (int i = 0; i < adressbook.GetLength(1); i++)
+            {
+                Console.WriteLine("{0,-12}{1}",adressbook[0, i],adressbook[1, i]);
+            }
+        }
+    }
+}
